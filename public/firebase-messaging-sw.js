ce1450947d23ef5eeefe6afb8d344ecc96af449d
Yentl Frickx <yentl.frickx@cegeka.com>
2023-03-28 11:14:05 +0200

    adjust serviceworker to send payload to temporary dump

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,6 +18,14 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    fetch('https://httpdump.app/dumps/33259392-4072-46fa-ad2f-3455a2798f51', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: payload
+    })
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
